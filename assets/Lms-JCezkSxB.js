936c1bb3fea5635ae07d385601c7a4c1b66095ed
etreeAssociation <messageetree@gmail.com>
2025-02-04 16:17:08 +0530

    update

--- assets/Lms-JCezkSxB.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-DdC8J5e4.js";function r(){return n.jsx(n.Fragment,{children:n.jsx("h1",{children:"Learning Management System Coming soon ... "})})}export{r as default};
--- assets/StudentPortal-Bv_BUhng.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DdC8J5e4.js";function r(){return t.jsx(t.Fragment,{children:t.jsx("h1",{children:"Student Portal Coming Soon ..."})})}export{r as default};
